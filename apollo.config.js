9404ae22667b42dc2cdbbaadc5b622700bfd3e95
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2025-01-24 22:10:15 +0200

    chore: configure Apollo Client and add apollo.config.js

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.export = {
+  client: {
+    service: {
+      name: "graphql-vue",
+      url: "http://localhost:8080/graphql",
+    },
+    includes: ["src/**/*.vue", "src/**/*.js"],
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, provide, h } from "vue";
+import App from "./App.vue";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create an apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
+
+app.mount("#app");
