a357ebef35b61bbf70a3d8a17eebb55b13e1b30f
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2025-01-24 23:09:17 +0200

    feat: implement user list display with loading and error states

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>GraphQL + Vue3</h1>
+  <div>
+    <h1>GraphQL + Vue3</h1>
+    <h1>Users List</h1>
+
+    <!-- Loading State -->
+    <p v-if="loading">Loading...</p>
+
+    <!-- Error State -->
+    <p v-if="error">Error: {{ error.message }}</p>
+
+    <!-- Data Display -->
+    <ul v-if="data">
+      <li v-for="user in data.users" :key="user.id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+  import { ref } from "vue";
+  import { useQuery } from "@vue/apollo-composable";
+  import { GET_USERS } from "./graphql/queries";
+
+  // Fetch data using useQuery
+  const { result, loading, error } = useQuery(GET_USERS);
+
+  // Convert result to a reactive ref
+  const data = ref(result);
+</script>
